b7c816fadb68699f8b90fd290ca20815352f5531
Kenny Chong <kennychong89@gmail.com>
2017-01-02 12:23:01 -0500

    Update

--- todo.rb
+++ todo.rb
@@ -12,6 +12,12 @@ def error_for_list_name(name)
   end
 end
 
+def error_for_todo_name(name)
+  if !(1..100).cover? name.size
+    "Todo name must be between 1 and 100 characters."
+  end
+end
+
 configure do
   enable :sessions
   set :sessions_secret, 'secret'
@@ -35,12 +41,14 @@ get "/lists/new" do
 end
 
 get "/lists/:id" do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
   erb :list, layout: :layout
 end
 
 get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
   erb :edit_list, layout: :layout
 end
 
@@ -55,5 +63,49 @@ post "/lists" do
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
+  end
+end
+
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updateed."
+    redirect "/lists/#{@list_id}"
+  end  
+end
+
+post "/lists/:id/delete" do
+  id = params[:id].to_i
+
+  if !session[:lists][id].nil?
+    session[:lists].delete_at(id)
+    session[:success] = "The list has been deleted."
+    redirect "/lists"    
+  else
+    session[:error] = "Cannot find the list."
+  end
+end
+
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  todo_name = params[:todo].strip
+  error = error_for_todo_name(todo_name)
+  @list = session[:lists][@list_id]
+
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: todo_name, completed: false}
+    session[:success] = "Todo has been added to the list."
+    redirect "/lists/#{@list_id}"
   end
 end
